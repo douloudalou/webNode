a197b0129c0989ccb7fe4aa39deeb29d05515a7b
douloudalou <kahjunt@gmail.com>
2023-03-16 18:03:17 +0800

    /admins/search

--- routes/web/home.js
+++ routes/web/home.js
@@ -133,7 +133,7 @@ route.post("/admins/login", function (req, res) {
 })
 
 // main page
-route.post('/admins/After/search', function (req, res) {
+route.post('/admins/search', function (req, res) {
     let tab = req.body.search_tab
     let title = req.body.search_title
     let item = req.body.search_item
