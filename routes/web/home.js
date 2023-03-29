083af2532788a61e3c63822f781583f00707c8d1
douloudalou <kahjunt@gmail.com>
2023-03-29 21:24:52 +0800

    testing

--- routes/web/home.js
+++ routes/web/home.js
@@ -585,53 +585,19 @@ route.post('/admins/After/newceptors_detail', function(req, res) {
 })
 
 // Invoice
-// route.post('/admins/invoice', function(req, res) {
-//     // details
-//     invoice_details = {
-//         parents_name: req.body.parents_name,
-//         ceptees_name: req.body.ceptees_name,
-//         address: req.body.address
-//     }
-//     // Invoice data
-//     var easyinvoice = require('easyinvoice');
-//     let fs = require('fs')
-//     var data = {
-//         //"documentTitle": "RECEIPT", //Defaults to INVOICE
-//         "currency": "SGD",
-//         "taxNotation": "gst", //or gst
-//         "marginTop": 25,
-//         "marginRight": 25,
-//         "marginLeft": 25,
-//         "marginBottom": 25,
-//         "logoExtension": "../webNode/views/small lbsx.png", //or base64
-//         //"logoExtension": "png", //only when logo is base64
-//         "sender": {
-//             "company": "SwimPerceptors",
-//             "country": "Singapore"
-//         },
-//         "client": {
-//             "Parent": `${details.parents_name}`,
-//             "Perceptees": `${details.ceptees_name}`,
-//             "Address": `${details.address}`
-//         },
-//         "invoiceNumber": "2020.0001",
-//         "invoiceDate": "05-01-2020",
-//         "products": [
-//             {
-//                 "quantity": "4",
-//                 "description": "Lessons completed",
-//                 "price": 50
-//             }
-//         ],
-//         "bottomNotice": "Kindly pay your invoice within 15 days."
-//     };
-//     easyinvoice.createInvoice(data, function (result) {
-//         //The response will contain a base64 encoded PDF file
-//         fs.writeFileSync("invoice.pdf", result.pdf, 'base64');
-//     });
+route.post('/admins/invoice', function(req, res) {
+    // details
+    invoice_details = {
+        parents_name: req.body.parents_name,
+        ceptees_name: req.body.ceptees_name,
+        address: req.body.address
+    }
+    
+    // Invoice url
+    fetch(filepath).then((response) => response.json()).then((data) => wf(data));
 
-//     load(req, res)
-// })
+    load(req, res)
+})
 
 // reload
 route.post('/admins/reload', function(req, res) {
---
+++ views/After_login/invoice.js
@@ -0,0 +1,37 @@
+var easyinvoice = require('easyinvoice');
+let fs = require('fs')
+let details = require('../../routes/web/home.js')
+var data = {
+//"documentTitle": "RECEIPT", //Defaults to INVOICE
+"currency": "SGD",
+"taxNotation": "gst", //or gst
+"marginTop": 25,
+"marginRight": 25,
+"marginLeft": 25,
+"marginBottom": 25,
+"logoExtension": "../../webNode/views/small lbsx.png", //or base64
+//"logoExtension": "png", //only when logo is base64
+"sender": {
+    "company": "SwimPerceptors",
+    "country": "Singapore"
+},
+"client": {
+    "Parent": `${details.rents_name}`,
+    "Perceptees": `${document.getElementsById('ceptees_name').value}`,
+    "Address": `${document.getElementsById('address').value}`
+},
+"invoiceNumber": "2020.0001",
+"invoiceDate": "05-01-2020",
+"products": [
+    {
+        "quantity": "4",
+        "description": "Lessons completed",
+        "price": 50
+    }
+],
+"bottomNotice": "Kindly pay your invoice within 15 days."
+};
+easyinvoice.createInvoice(data, async function (result) {
+    //The response will contain a base64 encoded PDF file
+    await fs.writeFileSync("invoice.pdf", result.pdf, 'base64');
+});
