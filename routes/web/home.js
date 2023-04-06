cd5d29a2418c3d0dd067c4a5b7c61e0eb0c72583
douloudalou <kahjunt@gmail.com>
2023-04-06 20:08:10 +0800

    const

--- routes/web/home.js
+++ routes/web/home.js
@@ -522,9 +522,8 @@ route.post('/admins/After/newceptors_detail', function(req, res) {
 let n=0
 route.post('/admins/invoice', function(req, res) {
     // import
-    let puppeteer = require('puppeteer')
     let {savePageAsPDF} = require('./invoice.js')
-    let fs = require('fs').promises;
+    const fs = require('fs').promises;
     // details
     var invoice_num = `SP${n}`
     invoice_details = {
