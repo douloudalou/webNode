a292b701c0fe3b9ad4f987501ee4b1f3fcbc03e3
douloudalou <kahjunt@gmail.com>
2023-04-06 21:58:49 +0800

    wf

--- routes/web/invoice.js
+++ routes/web/invoice.js
@@ -1,4 +1,15 @@
 const puppeteer = require('puppeteer');
+const {writeFile} = require('fs')
+
+function wf(content) {
+    let date = new Date();
+
+    now  = (new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'medium', timeZone: 'Singapore' }).format(date));
+
+    writeFile(`NodeConsole.txt`, `{${now}} ${content}\n`, { flag: 'a+' }, err => {
+        if (err) throw `err: ${err}`
+    })
+}
 
 async function savePageAsPDF(html, path) {
   const browser = await puppeteer.launch();
@@ -6,6 +17,7 @@ async function savePageAsPDF(html, path) {
   await pdfPage.setContent(html);
   await pdfPage.pdf({ path: path, format: 'A4' });
   await browser.close();
+  wf(`done generating invoice`)
 }
 
 module.exports = { savePageAsPDF };
