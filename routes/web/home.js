46c3a977b873cb650e78e4a3b72fff96e9d241ab
douloudalou <kahjunt@gmail.com>
2023-04-11 19:15:25 +0800

    session

--- routes/web/home.js
+++ routes/web/home.js
@@ -97,6 +97,30 @@ function wf(content) {
 route.use(express.urlencoded())
 route.use(express.json())
 
+// session 
+function setSessionTimeout(req) {
+    // Clear any existing timeout
+    if (req.session.timeout) {
+      clearTimeout(req.session.timeout);
+    }
+    
+    // Set new timeout for 5 minutes
+    req.session.timeout = setTimeout(() => {
+      // Destroy the session and redirect to login page
+        req.session.destroy((err) => {
+            if (err) {
+            wf(`err: ${err}`);
+            } else {
+                wf(`${req.session.user} timed out`)
+                res.redirect('/admins');
+            }
+        });
+    }, 5 * 60 * 1000); // 5 minutes in milliseconds
+}
+route.use((req, res, next) => {
+    setSessionTimeout(req);
+    next();
+  });
 
 // Swimperceptors page
 // route.get('/', function (req, res) {
@@ -138,30 +162,6 @@ route.post("/admins/login", function (req, res) {
     })
 })
 
-// session 
-function setSessionTimeout(req) {
-    // Clear any existing timeout
-    if (req.session.timeout) {
-      clearTimeout(req.session.timeout);
-    }
-    
-    // Set new timeout for 5 minutes
-    req.session.timeout = setTimeout(() => {
-      // Destroy the session and redirect to login page
-        req.session.destroy((err) => {
-            if (err) {
-            wf(`err: ${err}`);
-            } else {
-                wf(`${req.session.user} timed out`)
-            res.redirect('/admins');
-            }
-        });
-    }, 5 * 60 * 1000); // 5 minutes in milliseconds
-}
-route.use((req, res, next) => {
-    setSessionTimeout(req);
-    next();
-  });
 
 // main page
 route.post('/admins/search', function (req, res) {
