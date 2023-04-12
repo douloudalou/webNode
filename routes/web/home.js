63af54fb515dfcf5bdec41a32e8bba3cc8d4eb6a
douloudalou <kahjunt@gmail.com>
2023-04-12 16:31:40 +0800

    wf

--- routes/web/home.js
+++ routes/web/home.js
@@ -102,7 +102,6 @@ const timezone = 'Asia/Singapore';
 // middleware to set session timeout
 const setSessionTimeout = (req) => {
     req.session.cookie.expires = moment().tz(timezone).add(1, 'minutes').toDate()
-    wf(`${req.session.user}`)
     wf(`setSessionTimeout: ${req.session.cookie.expires}`)
 };
 
