7b47607dc89de9406b5ac804d95aab00dae25ac8
douloudalou <kahjunt@gmail.com>
2023-04-12 21:15:25 +0800

    if

--- routes/web/home.js
+++ routes/web/home.js
@@ -121,7 +121,7 @@ function checkSessionTimeout(req, res, next) {
 
 // middleware to apply setSessionTimeout and checkSessionTimeout for each incoming request
 route.use((req, res, next) => {
-    if (req.session.views) {
+    if (req.session.views >= 1) {
         wf(`${req.session.views}`, `${req.session.user}`)
         checkSessionTimeout(req, res, next);
     }
